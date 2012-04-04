c204170bbde232e80885f384aeea1fc1ebff7697
luoq <luoq08@gmail.com>
2012-04-04 21:31:23 +0800

    Add old note.R

---
+++ note.R
@@ -0,0 +1,22 @@
+#show counts of essays by essay_set
+table(train_set$essay_set)
+#show summary of essays by essay_set
+tapply(train_set$domain1_score,train_set$essay_set,summary)
+#show class of each column
+sapply(train_set,class)
+#convert a string to apply scan
+tc <- textConnection(file)
+#timer
+system.time(expression)
+#record current call
+match.call()
+#go to browser when error
+options(error=recover)
+#unlist
+unlist
+#get name of object
+name <- deparse(substitute(object))
+#get object by name
+object <- get(name)
+#R remote search
+RSiteSearch(string)

7bd99f2bdd6e8f34fb6d9cba1bf298b938ae84b9
luoq <luoq08@gmail.com>
2012-04-04 21:14:08 +0800

    Store data per essay set

--- prepare_data.R
+++ prepare_data.R
@@ -6,25 +6,24 @@ valid_set$essay_set <- factor(valid_set$essay_set)
 numberOfEssaySet <- 8
 
 require(tm)
-train_corpus <- lapply(1:numberOfEssaySet,function(k)
-                       Corpus(VectorSource(with(train_set,essay[essay_set==k]))))
-valid_corpus <- lapply(1:numberOfEssaySet,function(k)
-                       Corpus(VectorSource(with(valid_set,essay[essay_set==k]))))
-train_score <- NULL
-for(k in c(1,3,4,5,6)){
-  train_score[[k]] <- data.frame(rubric=with(train_set,domain1_score[essay_set==k]))
+Set=vector(mode="list",length=numberOfEssaySet)
+for( k in 1:numberOfEssaySet){
+  Set[[k]]$train_corpus <- Corpus(VectorSource(with(train_set,essay[essay_set==k])))
+  Set[[k]]$valid_corpus <- Corpus(VectorSource(with(valid_set,essay[essay_set==k])))
 }
-rm(k)
+for(k in c(1,3,4,5,6)){
+  Set[[k]]$train_score <- data.frame(rubric=with(train_set,domain1_score[essay_set==k]))
+}
 
-train_score[[2]] <- with(train_set,data.frame(rubric1=domain1_score[essay_set==2],rubric2=domain2_score[essay_set==2]))
+Set[[2]]$train_score <- with(train_set,data.frame(rubric1=domain1_score[essay_set==2],rubric2=domain2_score[essay_set==2]))
 ### sum of 4 traits are final score
-train_score[[7]] <- with(train_set[train_set$essay_set==7,-3],
+Set[[7]]$train_score <- with(train_set[train_set$essay_set==7,-3],
                          data.frame(Ideas=rater1_trait1+rater2_trait1,
                                     Organization=rater1_trait2+rater2_trait2,
                                     Style=rater1_trait3+rater2_trait3,
                                     Conventions=rater1_trait4+rater2_trait4))
 ### IdeasAndContent+Organization+SentenceFluency+2*Conventions is final score
-train_score[[8]] <- with(train_set[train_set$essay_set==8,-3],
+Set[[8]]$train_score <- with(train_set[train_set$essay_set==8,-3],
                          local({
                            result <- matrix(0,nrow=length(rater1_trait1),ncol=4)
                            mask <- is.na(rater3_trait1)
@@ -37,4 +36,4 @@ train_score[[8]] <- with(train_set[train_set$essay_set==8,-3],
                            result
                          }))
 
-rm(train_set,valid_set)
+rm(train_set,valid_set,k)

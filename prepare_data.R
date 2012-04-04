d27faf1a750741b6e69c749f7061bca36db44280
luoq <luoq08@gmail.com>
2012-04-04 15:32:05 +0800

    train_score as data.frame

--- prepare_data.R
+++ prepare_data.R
@@ -11,16 +11,17 @@ train_corpus <- lapply(1:numberOfEssaySet,function(k)
 valid_corpus <- lapply(1:numberOfEssaySet,function(k)
                        Corpus(VectorSource(with(valid_set,essay[essay_set==k]))))
 train_score <- NULL
-train_score[[1]] <- with(train_set,domain1_score[essay_set==1])
-train_score[[2]] <- with(train_set,cbind(domain1_score[essay_set==2],domain2_score[essay_set==2]))
-colnames(train_score[[2]]) <- c("domain1","domain2")
-train_score[[3]] <- with(train_set,domain1_score[essay_set==3])
-train_score[[4]] <- with(train_set,domain1_score[essay_set==4])
-train_score[[5]] <- with(train_set,domain1_score[essay_set==5])
-train_score[[6]] <- with(train_set,domain1_score[essay_set==6])
-train_score[[7]] <- with(train_set[train_set$essay_set==7,-3],cbind(rater1_trait1+rater2_trait1,
-                                     rater1_trait2+rater2_trait2,rater1_trait3+rater2_trait3,rater1_trait4+rater2_trait4))
-colnames(train_score[[7]]) <- c("Ideas","Organization","Style","Conventions")
+for(k in c(1,3,4,5,6)){
+  train_score[[k]] <- data.frame(rubric=with(train_set,domain1_score[essay_set==1]))
+}
+rm(k)
+
+train_score[[2]] <- with(train_set,data.frame(rubric1=domain1_score[essay_set==2],rubric2=domain2_score[essay_set==2]))
+train_score[[7]] <- with(train_set[train_set$essay_set==7,-3],
+                         data.frame(Ideas=rater1_trait1+rater2_trait1,
+                                    Organization=rater1_trait2+rater2_trait2,
+                                    Style=rater1_trait3+rater2_trait3,
+                                    Conventions=rater1_trait4+rater2_trait4))
 train_score[[8]] <- with(train_set[train_set$essay_set==8,-3],
                          local({
                            result <- matrix(0,nrow=length(rater1_trait1),ncol=4)
@@ -29,6 +30,7 @@ train_score[[8]] <- with(train_set[train_set$essay_set==8,-3],
                                                   rater1_trait5[mask]+rater2_trait5[mask],rater1_trait6[mask]+rater2_trait6[mask])
                            result[!mask,] <- 
                              2*cbind(rater3_trait1[!mask],rater3_trait2[!mask],rater3_trait5[!mask],rater3_trait6[!mask])
+                           result <- as.data.frame(result)
+                           names(result) <- c("IdeasAndContent","Organization","SentenceFluency","Conventions")
                            result
                          }))
-colnames(train_score[[8]]) <- c("IdeasAndContent","Organization","SentenceFluency","Conventions")
